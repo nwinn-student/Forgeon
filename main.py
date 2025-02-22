cc040e7f670160c441b7d1e6438b6eedcf599d1c
Noah Winn <111249648+nwinn-student@users.noreply.github.com>
2025-02-22 21:33:28 +0000

    main.py now allows for debugging
    Added a debug option so testing can be done faster.  By default, nothing will change, but you can change DEBUG to True which allows modifications to py files and template html files to be applied to the page when it is refreshed.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from gevent.pywsgi import WSGIServer
+# If True, we can change main.py, app.py, and any of the template html files and they will automatically update the page when you refresh.  
+DEBUG = False
+
 from app import app
 
 addr = '127.0.0.1'
 port = 8080
 
-print(f'Starting application, open url: http://{addr}:{port}')
-http_server = WSGIServer((addr, port), app)
-http_server.serve_forever()
+if not DEBUG:
+	from gevent.pywsgi import WSGIServer
+	print(f'Starting application, open url: http://{addr}:{port}')
+	http_server = WSGIServer((addr, port), app)
+	http_server.serve_forever()
+else:
+	app.run(debug=True, host= addr, port=port)
